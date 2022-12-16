b217249889df28fcec266e4a6a3ae50e4c836022
axelcoch <axel.cochet@edu.esiee.fr>
2022-12-16 14:42:19 +0100

    Debut du front

--- quiz-api/app.py
+++ quiz-api/app.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 from flask_cors import CORS
 
 from jwt_utils import build_token, decode_token
+from model import 
 
 app = Flask(__name__)
 CORS(app)
@@ -23,6 +24,18 @@ def GetPassword():
         token = build_token()
         return {"token": token}, 200
     else:
+        return 'Unauthorized', 401
+
+@app.route('/questions', methods=['POST'])
+def AddQuestions():
+    try:
+        #Récupérer le token envoyé en paramètre
+        request.headers.get('Authorization')
+        #récupèrer un l'objet json envoyé dans le body de la requète
+        questions = request.get_json()
+        return {"questions": questions}, 200
+
+    except Exception:
         return 'Unauthorized', 401
 
 if __name__ == "__main__":
---
+++ quiz-api/model.py
@@ -0,0 +1,6 @@
+
+# Exemple de création de classe en python
+class Question():
+	def init(self, title: str, text: str, position: int):
+		self.title = title,
+        self.text = text,
