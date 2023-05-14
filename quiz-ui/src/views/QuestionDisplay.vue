e5f555318437f2b4331482f014fcf409b6f52272
axelcoch <axel.cochet@edu.esiee.fr>
2023-05-14 14:27:51 +0200

    amelioration front display question parce que bon faut que ça soit cohérent.

--- quiz-ui/src/views/QuestionDisplay.vue
+++ quiz-ui/src/views/QuestionDisplay.vue
@@ -1,23 +1,28 @@
 <template>
   <div
-    class="d-flex flex-column justify-content-center align-items-center w-80 text-dark"
+    class="d-flex flex-column justify-content-center align-items-center text-dark"
   >
-    <h1>{{ question.title }}</h1>
-    <h4>{{ question.text }}</h4>
-    <img v-if="question.image" :src="question.image" />
-
     <div
-      class="d-flex flex-column justify-content-center text-center my-5 w-50 p-3"
-      style="background-color: #eee; opacity: 0.7"
+      class="card d-flex flex-column justify-content-center align-items-center p-2"
+      style="opacity: 0.8; width: 60%; max-width: 60rem; min-width: 20rem;"
     >
-      <a
-        class="text-center text-dark link-dark text-decoration-none h-25"
-        style="font-size: 1.2em; cursor: pointer"
-        v-for="(answer, index) in question.possibleAnswers"
-        @click="$emit('answer-selected', index)"
+      <h1>{{ question.title }}</h1>
+      <h4>{{ question.text }}</h4>
+      <img v-if="question.image" :src="question.image" />
+
+      <div
+        class="d-flex flex-column justify-content-center text-center my-3 w-50 p-3"
+        style="background-color: #eee; opacity: 0.7"
       >
-        {{ answer.text }}
-      </a>
+        <a
+          class="text-center text-dark link-dark text-decoration-none h-25 rounded"
+          style="font-size: 1.2em; cursor: pointer"
+          v-for="(answer, index) in question.possibleAnswers"
+          @click="$emit('answer-selected', index)"
+        >
+          {{ answer.text }}
+        </a>
+      </div>
     </div>
   </div>
 </template>
